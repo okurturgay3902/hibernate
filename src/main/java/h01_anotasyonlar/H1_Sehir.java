679c859a8278e41944abeb6672744b8a8aa8921d
okurturgay3902 <94409068+okurturgay3902@users.noreply.github.com>
2022-01-13 22:27:08 +0100

    ders2

--- src/main/java/h01_anotasyonlar/H1_Sehir.java
+++ src/main/java/h01_anotasyonlar/H1_Sehir.java
@@ -1,91 +1,71 @@
 package h01_anotasyonlar;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-
-
-// ================================================================
+//================================================================
 //HIBERNATE ICIN TABLO TANIMLAMASININ YAPILMASI 
 //tablo oluşturabilmek için içerik hazırlama )alttakiler)
-//POJO: Plain Old Java Object									
+//POJO: Plain Old Java Object                                   
 //1) Private degiskenler tanimlanir.
 //2) constructor olusturulur.
 //3) Getter ve Setter metotlar tanimlanir.
 //4) toString() metodu ile nesne yazdirilabilir hale getirilir.
 //================================================================
-
-
 /*
-	Bir Class Hibernate ile tablo olusturmada kullanilacaksa degiskenlerin
-	“final” veya “static” tanimlanMAMAsi gerekir. 
+    Bir Class Hibernate ile tablo olusturmada kullanilacaksa degiskenlerin
+    “final” veya “static” tanimlanMAMAsi gerekir. 
 */
-
 //- @Entity anotasyonu bu class’ın bir tabloya donusturulmesi
 //gerektigini Hibernate’e gosterir.(sql ortamına mapping yapıo uyarlıo,translate)
 //Her bir degisken bir sutun olacaktir.
 //- @Table annotasyonu tablo adinin degistirilmesini saglar.javada ve sql deki isimler aynı olmak zorunda değil
 //- @Id anotasyonu tablo icerisinde primary key olusturur.
 //- @Transient anotasyonu bir degiskenin (sutun) tabloda yer almayacagini gosterir.
-
-
-
-@Entity  //altaki class ismiyle table olusturuyor
-@Table(name="sehir_tablosu") //zorunda degilim, yaparsam sql deki ismini vermis olurum
+@Entity   //alttaki class ismiyle table olusturuyor.
+@Table(name="Sehir_Tablosu") //zorunda degiliz, sql deki tablo ismini degistirmis olurum
 public class H1_Sehir {
-	
-	@Id  //bu sutun primary key yani null ve unique
-	@Column(name="sehir_Plaka")	//sutunun sql deki ismini sehir_plaka yaptik
-	private int sehirPlaka; //tablonun ilk sutununu olusturduk
-	
-	private String sehirAdi;
-	
-	private int sehirNufus;
-	
-	@Transient  // javada kullanabiliriz ama sql deki tabloda gozukmez
-	private String adres;
-	
-	
-	
-
-	
-	public H1_Sehir() {
-	
-	}
-
-public H1_Sehir(int i, String string, int j) {
-		// TODO Auto-generated constructor stub
-	}
-
-@Override
-public String toString() {
-	return "H1_Sehir [sehirPlaka=" + sehirPlaka + ", sehirAdi=" + sehirAdi + ", sehirNufus=" + sehirNufus
-			 + "]";
-}
-
-public int getSehirPlaka() {
-	return sehirPlaka;
-}
-
-public void setSehirPlaka(int sehirPlaka) {
-	this.sehirPlaka = sehirPlaka;
-}
-
-public String getSehirAdi() {
-	return sehirAdi;
-}
-
-public void setSehirAdi(String sehirAdi) {
-	this.sehirAdi = sehirAdi;
-}
-
-public int getSehirNufus() {
-	return sehirNufus;
-}
-
-public void setSehirNufus(int sehirNufus) {
-	this.sehirNufus = sehirNufus;
-}
+    @Id                                                  // bu sütun primary key=not null ve unique olacak.
+    @Column(name="sehir_plaka")     //sütunun sql deki ismini degistirdik
+    private int sehirPlaka;
+    
+    private String sehirAdi;
+    private int sehirNufus;
+    
+    @Transient            //burayi sqlde sütun olarak eklemez.. Javada kullanabiliriz ama SQLdeki tabloda görünmez..
+    private String adres;
+    public H1_Sehir(int sehirPlaka, String sehirAdi, int sehirNufus) {
+        this.sehirPlaka = sehirPlaka;
+        this.sehirAdi = sehirAdi;
+        this.sehirNufus = sehirNufus;   
+    }
+    public H1_Sehir() {
+    }
+    public int getSehirPlaka() {
+        return sehirPlaka;
+    }
+    public void setSehirPlaka(int sehirPlaka) {
+        this.sehirPlaka = sehirPlaka;
+    }
+    public String getSehirAdi() {
+        return sehirAdi;
+    }
+    public void setSehirAdi(String sehirAdi) {
+        this.sehirAdi = sehirAdi;
+    }
+    public int getSehirNufus() {
+        return sehirNufus;
+    }
+    public void setSehirNufus(int sehirNufus) {
+        this.sehirNufus = sehirNufus;
+    }
+    @Override
+    public String toString() {
+        return "H1_Sehir [sehirPlaka=" + sehirPlaka + ", sehirAdi=" + sehirAdi + ", sehirNufus=" + sehirNufus + "]";
+    }
+    
+    
+    
+    
 }
--- src/main/java/h01_anotasyonlar/H2_Save.java
+++ src/main/java/h01_anotasyonlar/H2_Save.java
@@ -1,10 +1,13 @@
 package h01_anotasyonlar;
 
+import java.util.Scanner;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+
 
 public class H2_Save {
 
@@ -12,37 +15,52 @@ public class H2_Save {
 		
 		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(H1_Sehir.class);
 		
-	//	cfg.xml dosyasindaki bilgilerle mysql e baglan							kalip class'imizi buraya cagir
+	//cfg.xml dosyasındaki bilgilerle mysql e bağlan                             kalıp class ımızı buraya çağır	
 		
 		
-		SessionFactory sf= con.buildSessionFactory();  //acacagimizin alanlarin parent'i alan fabrikasi
+		SessionFactory sf= con.buildSessionFactory(); // açacağımız alanların parent ı, alan fabrikası
 		
-		Session session=sf.openSession();  //alan fabrikasindan bir yer baslattik
+		Session session =sf.openSession(); //alan fabrikasından bir yer başlattık
+		
 		
 		// Acilan session icerisinde islemlere baslayabilmek icin Transaction aciyoruz.
-				/*Transactionlar ile bir işlem yarıda kaldıysa veya 
+				/*Transactionlar ile bir işlem yarıda kaldıysa  veya 
 				 * tam olarak tamamlanadıysa tüm adımlar başa alınır
 				 * veri ve işlem güvenliği için önemlidir.Kısacası ya hep 
 				 * ya hiç prensibine göre çalışır
 				 */
 		
-		
 		Transaction tx=session.beginTransaction();
 		
-		H1_Sehir sehir1 = new H1_Sehir(37, "Istanbul", 10000000);
+		H1_Sehir sehir1 = new H1_Sehir(34, "Istanbul", 10000000);
+		session.save(sehir1); //insert into
 		
-		session.save(sehir1);
+		session.save(new H1_Sehir(35,"Izmir",2500000));
 		
+		//dışarıdan veri girelim
 		
+		Scanner scan =new Scanner(System.in);
+		System.out.println("plaka giriniz");
+		int plaka=scan.nextInt();
 		
+		System.out.println("sehir giriniz");
+		String sehir=scan.next();
 		
-		tx.commit();  //islemlerin database hizli gitmesini saglar
+		System.out.println("nüfus giriniz");
+		int nüfus=scan.nextInt();
+		
+		session.save(new H1_Sehir(plaka,sehir,nüfus));
+		
+		tx.commit(); //işlemlerin db e hızlı gitmesini sağlar
+		
 		
 		sf.close();
 		session.close();
+		scan.close();
 		
 		
+		
+		
+	}	
 
 	}
-
-}
